36bd2f90fc6bcdadf0fdc514b281ad030e2e23a2
jluetgert <julian.luetgert@uni-rostock.de>
2026-09-10 13:37:42 +0200

    Allow for additional quantities to be saved

--- tools/create_s_ij_grid.py
+++ tools/create_s_ij_grid.py
@@ -16,13 +16,23 @@ number_fraction = [0.5, 0.5]
 qoz_pad_factor = 2
 qoz_linear_n_points = 2**12
 
-k_points = qoz_linear_n_points * qoz_pad_factor
+# Sample parameters
+T_e = np.linspace(5, 100, 1)
+rho = np.linspace(-0.5, 0.2, 1) + 1.51
+alpha = [1]
+
+# Output quantities. currently implemented:
+# Sii, q, f, Zbar, Zstar
+output = ["Sii", "q", "f", "Zbar", "Zstar"]
+
 ###
 
+k_points = qoz_linear_n_points * qoz_pad_factor
 
-def otter_calc(T_e, rho):
+
+def otter_calc(T_e, rho, alpha):
     """
-    Returns S_ii with shape (no_of_ions, no_of_ions, len(k)), k, and Z
+    Returns the full set of observables from the otter calculation
     """
     aa_overrides = {
         "bound_zero_tail_refine": True,
@@ -33,51 +43,95 @@ def otter_calc(T_e, rho):
     kwargs = {
         "qoz_pad_factor": qoz_pad_factor,
         "qoz_linear_n_points": qoz_linear_n_points,
+        "xc_model": "dirac",
+        # "hnc_bridge_model": "rosenfeld_ashcroft",
+        "aa_overrides": aa_overrides,
     }
     cfg = PlasmaWorkflowConfig(
         elements=elements,
         number_fraction=number_fraction,
         temperature_ev=T_e,
         rho_g_cc=rho,
-        ion_temperature_ev=T_e,
+        ion_temperature_ev=T_e * alpha,
         save_state_npz=False,
-        aa_overrides=aa_overrides,
         # electronic_model='tf',
         **kwargs,
     )
-    result = solve_plasma_workflow(cfg)["ion"]
-    return result["sij_k"], result["k"], result["zbar"]
+    res = solve_plasma_workflow(cfg)
+    ion_res = res["ion"]
+    e_res = res["electronic"]["result"]
+    if len(elements) == 1:
+        zbar = e_res["n0"] / ion_res["n_i"]
+    else:
+        n0 = [e_res["species"][idx]["result"]["n0"] for idx in range(len(elements))]
+        zbar = n0 / ion_res["n_i"]
+    return (
+        ion_res["k"],
+        ion_res["sij_k"],
+        ion_res["zbar"],
+        zbar,
+        ion_res["f_k"],
+        ion_res["q_k"],
+    )
 
 
-def calculate_job(n_idx, m_idx, T_e, rho):
+def calculate_job(n_idx, m_idx, p_idx, T_e, rho, alpha):
     """
     Worker function.
     """
-    result = otter_calc(T_e, rho)
-    return n_idx, m_idx, result
+    result = otter_calc(T_e, rho, alpha)
+    return n_idx, m_idx, p_idx, result
 
 
-def run(T_e, rho, filename, n_workers=None):
+def run(T_e, rho, alpha, filename, n_workers=None):
     n = len(T_e)
     m = len(rho)
+    p = len(alpha)
 
     with h5py.File(filename, "w") as f:
-        Sii = f.create_dataset(
-            "S_ii",
-            shape=(len(elements), len(elements), k_points, n, m),
-            dtype=np.float64,
-            chunks=(len(elements), len(elements), k_points, 1, 1),
-        )
-        Sii.attrs["axis"] = ["i", "j", "k", "T", "rho"]
-        Sii.attrs["unit"] = [""]
-
-        Zbar = f.create_dataset(
-            "Z_bar",
-            shape=(len(elements), n, m),
-            dtype=np.float64,
-        )
-        Zbar.attrs["axis"] = ["i", "T", "rho"]
-        Zbar.attrs["unit"] = [""]
+        if "Sii" in output:
+            Sii = f.create_dataset(
+                "S_ii",
+                shape=(len(elements), len(elements), k_points, n, m, p),
+                dtype=np.float64,
+                chunks=(len(elements), len(elements), k_points, 1, 1, 1),
+            )
+            Sii.attrs["axis"] = ["i", "j", "k", "T_e", "rho", "alpha"]
+            Sii.attrs["unit"] = [""]
+        if "f" in output:
+            f_k = f.create_dataset(
+                "f",
+                shape=(len(elements), k_points, n, m, p),
+                dtype=np.float64,
+                chunks=(len(elements), k_points, 1, 1, 1),
+            )
+            f_k.attrs["axis"] = ["i", "k", "T_e", "rho", "alpha"]
+            f_k.attrs["unit"] = [""]
+        if "q" in output:
+            q_k = f.create_dataset(
+                "q",
+                shape=(len(elements), k_points, n, m, p),
+                dtype=np.float64,
+                chunks=(len(elements), k_points, 1, 1, 1),
+            )
+            q_k.attrs["axis"] = ["i", "k", "T_e", "rho", "alpha"]
+            q_k.attrs["unit"] = [""]
+        if "Zbar" in output:
+            Zbar = f.create_dataset(
+                "Z_bar",
+                shape=(len(elements), n, m, p),
+                dtype=np.float64,
+            )
+            Zbar.attrs["axis"] = ["i", "T", "rho, alpha"]
+            Zbar.attrs["unit"] = [""]
+        if "Zstar" in output:
+            Zstar = f.create_dataset(
+                "Z_star",
+                shape=(len(elements), n, m, p),
+                dtype=np.float64,
+            )
+            Zstar.attrs["axis"] = ["i", "T", "rho, alpha"]
+            Zstar.attrs["unit"] = [""]
 
         axis = f.create_group("axis")
 
@@ -104,6 +158,15 @@ def run(T_e, rho, filename, n_workers=None):
         rho_out[:] = rho
         rho_out.attrs["unit"] = ["g/cc"]
 
+        alpha_out = axis.create_dataset(
+            "alpha",
+            shape=(p),
+            dtype=np.float64,
+        )
+        alpha_out[:] = alpha
+        alpha_out.attrs["unit"] = [""]
+        alpha_out.attrs["definition"] = ["T_i/T_e"]
+
         element_out = axis.create_dataset(
             "elements",
             shape=(len(elements)),
@@ -115,30 +178,35 @@ def run(T_e, rho, filename, n_workers=None):
 
         with ProcessPoolExecutor(max_workers=n_workers) as executor:
             futures = {
-                executor.submit(
-                    calculate_job,
+                executor.submit(calculate_job, n_idx, m_idx, p_idx, t, r, a): (
                     n_idx,
                     m_idx,
-                    t,
-                    r,
-                ): (n_idx, m_idx)
+                    p_idx,
+                )
                 for n_idx, t in enumerate(T_e)
                 for m_idx, r in enumerate(rho)
+                for p_idx, a in enumerate(alpha)
             }
 
             for future in as_completed(futures):
-                n_idx, m_idx, result = future.result()
+                n_idx, m_idx, p_idx, result = future.result()
+                res_k, res_Sii, res_Zbar, res_Zstar, res_q, res_f = result
 
-                Sii[:, :, :, n_idx, m_idx] = result[0]
-                k_out[:] = result[1]
-                Zbar[:, n_idx, m_idx] = result[2]
+                k_out[:] = res_k
+                if "Sii" in output:
+                    Sii[:, :, :, n_idx, m_idx, p_idx] = res_Sii
+                if "Zbar" in output:
+                    Zbar[:, n_idx, m_idx, p_idx] = res_Zbar
+                if "Zstar" in output:
+                    Zstar[:, n_idx, m_idx, p_idx] = res_Zstar
+                if "q" in output:
+                    q_k[:, :, n_idx, m_idx, p_idx] = res_q
+                if "f" in output:
+                    f_k[:, :, n_idx, m_idx, p_idx] = res_f
                 f.flush()
 
                 print(f"Finished [{n_idx}, {m_idx}] ({len(result)=})")
 
 
 if __name__ == "__main__":
-    T_e = np.linspace(5, 100, 4)
-    rho = np.linspace(-0.5, 0.2, 4) + 1.51
-
-    run(T_e, rho, "output2.hdf5")
+    run(T_e, rho, alpha, "output.hdf5")
